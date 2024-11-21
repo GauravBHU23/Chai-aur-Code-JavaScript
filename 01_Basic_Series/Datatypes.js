7da231ef5d8b50dc1e6fac0cd8bb92341cf0f1da
Gaurav Kumar <149852863+GauravBHU23@users.noreply.github.com>
2024-11-21 17:52:38 +0000

    DataTypes.js

---
+++ 01_Basic_Series/Datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code
+//alert("hello");
+
+console.log(3+3);
+console.log("Gaurav");
+
+let name = "Gaurav";
+let age = 18;
+let isLoggedIn = false;
+console.log(typeof null);
+
---
+++ 01_Basic_Series/Variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "Gaurav123@gmail.com";
+var accountPassword = "12345";
+accountCity = "Varanasi";
+
+// accountId = 2; // not allowed
+
+accountEmail = "GauravKum1275@gmail.com";
+accountPassword = "232323";
+accountCity = "Patna";
+let accountState;
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
